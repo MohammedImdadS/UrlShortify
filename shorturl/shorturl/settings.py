1291076753fce56d5d00ea9ef4a8d09ce08d10ff
MohammedImdad <imdadsmd@gmail.com>
2020-08-11 08:59:43 +0530

    update requirements

--- shorturl/shorturl/settings.py
+++ shorturl/shorturl/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+import django_heroku
 import os
 import dotenv
 
@@ -126,3 +126,6 @@ STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
+
+#Activate Django-Heroku
+django_heroku.settings(locals())
